8e3bf44315497adc20eba60e3e30793206a27dad
Rafael-Mal-Cas <rafael.malagon@sm-services.es>
2025-05-07 10:11:59 +0200

    Update MiServlet.java

--- TestWeb/src/main/java/servlets/MiServlet.java
+++ TestWeb/src/main/java/servlets/MiServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.mindrot.jbcrypt.BCrypt;
-
+//Uso de git
 
 @WebServlet("/MiServlet") 
 public class MiServlet extends HttpServlet {
